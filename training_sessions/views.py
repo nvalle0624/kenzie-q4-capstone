cba9afdce971e320f83e23fba8118f0f5b64f086
Anthony Alvarez <anthonyalvarez@Anthonys-iMac.home>
2021-07-12 10:44:36 -0400

    finished the report view and template

--- training_sessions/views.py
+++ training_sessions/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from training_sessions.models import Report
 
-# Create your views here.
+# Create your views here
+
+def reports(request):
+    report = Report.objects.all()
+    return render(request, 'reports.html', {'report':report})
