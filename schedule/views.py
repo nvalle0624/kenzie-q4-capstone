1d73cc5dab24975cee35d56aed7aac816aa2428d
EliTheNut <elijah.webster.biz@gmail.com>
2021-07-12 13:42:36 -0400

    calendar psuedocode

--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def calendar(request):
+    ''' 
+    if user == admin:
+        sessions = sessions.object.all()
+        return render(request, 'schedule.html', {'sessions':sessions})  
+    elif user == trainer:
+        User = user.id
+        sessions = sessions.object.filter(trainer=User)
+        return render(request, 'schedule.html', {'sessions':sessions})   
+    else:
+        User = user.id
+        appointments = sessions.object.filter(user=User)
+        return render(request, 'schedule.html', {'sessions':sessions})
+    '''
