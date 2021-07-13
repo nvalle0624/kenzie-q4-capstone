570fdf726bcf61cd4a80cf4016573820db36f43d
nvalle0624 <nvalle0624@gmail.com>
2021-07-13 12:27:45 -0400

    add calendar model

--- training_sessions/urls.py
+++ training_sessions/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from training_sessions import views
 
 
-
-
 urlpatterns = [
-    path('reports/',views.reports, name='reports' ),
-    
+    path('reports/', views.reports, name='reports'),
+    path(r'^calendar/$', views.Calendar.as_view(), name='calendar'),
 ]
