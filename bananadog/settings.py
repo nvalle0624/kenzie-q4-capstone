8361693ab2315d3fc896c14217075f1d0795ccaa
nvalle0624 <nvalle0624@gmail.com>
2021-07-13 14:45:28 -0400

    css linked to base/calender html

--- bananadog/settings.py
+++ bananadog/settings.py
@@ -137,13 +137,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
