0522262bd57c638f64e0f714382bf9c158fe6545
nvalle0624 <nvalle0624@gmail.com>
2021-07-13 12:09:40 -0400

    login, auth, redirects work per client/trainer

--- admin_users/views.py
+++ admin_users/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, HttpResponseRedirect, reverse, HttpResponse
 from admin_users.models import Trainer
 from admin_users.forms import TrainerForm
 from django.contrib.auth.models import User
+from users.forms import SignUpForm
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
 
 
 # Create your views here.
@@ -11,11 +13,28 @@ from django.contrib.auth.decorators import login_required
 
 
 @staff_member_required
-def trainer_home(request):
+def trainer_home(request, user_id: int):
     if request.user.is_authenticated:
-        trainer = Trainer.objects.get(id=request.user.id)
+        trainer = Trainer.objects.get(admin_user=request.user)
         return render(request, 'admin_homepage.html', {'trainer': trainer})
     return HttpResponseRedirect(reverse('add_trainer'))
+
+
+@staff_member_required
+def add_admin_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        print('Form', form.errors)
+        if form.is_valid():
+            data = form.cleaned_data
+            new_admin_user = User.objects.create_user(
+                username=data["username"],
+                email=data['email'],
+                password=data["password"],
+                is_staff=True)
+            return HttpResponseRedirect(reverse('login'))
+    form = SignUpForm()
+    return render(request, "clientform.html", {"form": form})
 
 
 @staff_member_required
@@ -36,6 +55,6 @@ def add_trainer(request):
 
             )
 
-            return HttpResponseRedirect(reverse("homepage"))
+            return HttpResponseRedirect(reverse("trainer_home", args=[new_trainer.id]))
     form = TrainerForm()
     return render(request, "trainer_form.html", {"form": form})
