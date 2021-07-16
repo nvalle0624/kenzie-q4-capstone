9f690090eae4611a92f2f5383cc703f199b9e00b
nvalle0624 <nvalle0624@gmail.com>
2021-07-16 09:23:11 -0400

    all messages and sending messages now on one page

--- all_messages/views.py
+++ all_messages/views.py
@@ -29,8 +29,9 @@ def all_messages_view(request, user_id: int):
         count += 1
 
     user_filter = []
-    if request.method == 'POST' and this_user.is_staff == False:
-        form = ClientMessageFilterForm(request.POST)
+
+    if request.method == 'GET' and this_user.is_staff == False:
+        form = ClientMessageFilterForm(request.GET)
         if form.is_valid():
             data = form.cleaned_data
             if len(user_filter) > 0:
@@ -45,9 +46,24 @@ def all_messages_view(request, user_id: int):
                                                          'all_trainers': all_trainers,
                                                          'user_filter': user_filter[0],
                                                          })
+    elif request.method == 'POST' and this_user.is_staff == False:
+        form2 = ClientMessageForm(request.POST)
+        if form2.is_valid():
+            data = form2.cleaned_data
+            new_message = Message.objects.create(
+                text=data['text'],
+                sent_by=this_user,
+                send_to=data['send_to']
+            )
+            new_notification = Notification.objects.create(
+                text=data['text'],
+                sent_by=this_user,
+                send_to=data['send_to']
+            )
+            return HttpResponseRedirect(reverse('all_messages_view', args=[this_user.id]))
 
-    elif request.method == 'POST' and this_user.is_staff == True:
-        form = TrainerMessageFilterForm(request.POST)
+    elif request.method == 'GET' and this_user.is_staff == True:
+        form = TrainerMessageFilterForm(request.GET)
         if form.is_valid():
             data = form.cleaned_data
             if len(user_filter) > 0:
@@ -55,26 +71,48 @@ def all_messages_view(request, user_id: int):
                 user_filter.append(data['name'])
             else:
                 user_filter.append(data['name'])
+            form2 = TrainerMessageForm()
             return render(request, 'all_messages.html', {'all_messages': all_messages,
                                                          'all_notifications': all_notifications,
                                                          'count': count,
                                                          'form': form,
                                                          'all_trainers': all_trainers,
-                                                         'user_filter': user_filter[0]})
+                                                         'user_filter': user_filter[0],
+                                                         'form2': form2})
+    elif request.method == 'POST' and this_user.is_staff == True:
+        form2 = TrainerMessageForm(request.POST)
+        if form2.is_valid():
+            data = form2.cleaned_data
+            new_message = Message.objects.create(
+                text=data['text'],
+                sent_by=this_user,
+                send_to=data['send_to']
+            )
+            new_notification = Notification.objects.create(
+                text=data['text'],
+                sent_by=this_user,
+                send_to=data['send_to']
+            )
+            return HttpResponseRedirect(reverse('all_messages_view', args=[this_user.id]))
+
     if this_user.is_staff != True:
         form = ClientMessageFilterForm()
+        form2 = ClientMessageForm()
         return render(request, 'all_messages.html', {'all_messages': all_messages,
                                                      'all_notifications': all_notifications,
                                                      'count': count,
                                                      'all_trainers': all_trainers,
-                                                     'form': form})
+                                                     'form': form,
+                                                     'form2': form2})
     else:
         form = TrainerMessageFilterForm()
+        form2 = TrainerMessageForm()
         return render(request, 'all_messages.html', {'all_messages': all_messages,
                                                      'all_notifications': all_notifications,
                                                      'count': count,
                                                      'all_trainers': all_trainers,
-                                                     'form': form})
+                                                     'form': form,
+                                                     'form2': form2})
 
 
 # @login_required
@@ -82,9 +120,9 @@ def client_message_form_view(request):
     all_trainers = Trainer.objects.all()
     this_user = User.objects.get(id=request.user.id)
     if request.method == 'POST' and this_user.is_staff == False:
-        form = ClientMessageForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
+        form2 = ClientMessageForm(request.POST)
+        if form2.is_valid():
+            data = form2.cleaned_data
             new_message = Message.objects.create(
                 text=data['text'],
                 sent_by=this_user,
@@ -98,9 +136,9 @@ def client_message_form_view(request):
             return HttpResponseRedirect(reverse('all_messages_view', args=[this_user.id]))
 
     elif request.method == 'POST' and this_user.is_staff == True:
-        form = TrainerMessageForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
+        form2 = TrainerMessageForm(request.POST)
+        if form2.is_valid():
+            data = form2.cleaned_data
             new_message = Message.objects.create(
                 text=data['text'],
                 sent_by=this_user,
@@ -114,9 +152,9 @@ def client_message_form_view(request):
             return HttpResponseRedirect(reverse('all_messages_view', args=[this_user.id]))
     if this_user.is_staff != True:
         this_client = Client.objects.get(user=this_user)
-        form = ClientMessageForm()
-        return render(request, 'message_form.html', {'form': form, 'this_client': this_client, 'all_trainers': all_trainers})
+        form2 = ClientMessageForm()
+        return render(request, 'all_messages.html', {'form2': form2, 'this_client': this_client, 'all_trainers': all_trainers})
     else:
         this_trainer = Trainer.objects.get(admin_user=this_user)
-        form = TrainerMessageForm()
-        return render(request, 'message_form.html', {'form': form, 'this_trainer': this_trainer, 'all_trainers': all_trainers})
+        form2 = TrainerMessageForm()
+        return render(request, 'all_messages.html', {'form2': form2, 'this_trainer': this_trainer, 'all_trainers': all_trainers})
