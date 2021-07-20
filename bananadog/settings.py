1dce1457fd5e2b50fbb67849ac11a77a0c8068f5
Anthony Alvarez <anthonyalvarez@Anthonys-iMac.home>
2021-07-20 13:51:24 -0400

    error hanling implimented

--- bananadog/settings.py
+++ bananadog/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_jlul8g10rsxtrr$d978i&c@q%%)cj*obc#j1=47oxqpn65^ar'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
