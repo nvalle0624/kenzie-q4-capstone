2f4c3159188085407ca963f63a5ac828d80bf26b
nvalle0624 <nvalle0624@gmail.com>
2021-09-09 13:38:49 -0400

    moved key

--- bananadog/settings.py
+++ bananadog/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-# SECRET_KEY = 'django-insecure-_jlul8g10rsxtrr$d978i&c@q%%)cj*obc#j1=47oxqpn65^ar'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
